e408ec450bd57476d2f2b9e32e725c44917a1db4
Fariba-Kamani <fariba.kamani87@gmail.com>
2024-11-06 10:32:24 +0000

    Add input validation to check the user enters only digits, uses exactly 10 digits for personal ID number and 6 digits for the PIN code

--- run.py
+++ run.py
@@ -12,6 +12,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Bank_account')
 
-user_details = SHEET.worksheet('user')
+user_details = SHEET.worksheet('user_details')
 data = user_details.get_all_values()
-print(data)
+
+def login():
+    print("_______________________WELCOME!_______________________\n")
+    print(" Please enter your personal ID number and your PIN code to login.")
+    print(" -Personal ID number should be 10 digits. Example: 8909091234, format: YYMMDD****")
+    print(" -PIN code should be exactly 6 digits. Example: 123456")
+    print("______________________________________________________")
+    personal_ID = input("Enter your personal ID number here, 10 digits:\n")
+    pin_code = input("Enter your PIN code here, 6 digits:\n")
+    try:
+        if not personal_ID.isdigit() or not pin_code.isdigit():
+            raise ValueError(
+                f"Your personal ID and PIN code should only include digits!"
+            )
+        elif len(personal_ID) != 10 or len(pin_code) != 6:
+            raise ValueError(
+                f"Your personal ID number should be exactly 10 digits, \nand your PIN code should be exactly 6 digits."
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e} Please try again.\n")
+
+login()
