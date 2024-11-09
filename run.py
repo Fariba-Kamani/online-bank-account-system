b29239c811386ee298f4b7e43d33d70287699504
Seyedehfariba Kamani <fariba.kamani87@gmail.com>
2024-11-09 19:26:23 +0000

    Fix exception handlings, modify deposit and withdra methods within the BankAccount class, and add two mmethods to the class for handling the transactions and balance update in the corresponding worksheet.

--- run.py
+++ run.py
@@ -23,14 +23,15 @@ class BankAccount:
     Bank account class
     """
    
-    def __init__(self, first_name, surname, pin_code, id_number, account_number, balance=0):
+    def __init__(self, first_name, surname, pin_code, id_number, account_number, row_number, balance=0):
         # Creates an instance of bank account
         self.first_name = first_name
         self.surname = surname
         self.pin_code = pin_code
         self.id_number = id_number
         self.account_number = account_number
-        self.balance = balance
+        self.balance = round(balance, 2)
+        self.row_number = row_number
         self.transactions = []
     
     def welcome_message(self):
@@ -51,22 +52,76 @@ class BankAccount:
                 print("Invalid response! Please answer only 1 for menu or 2 to log out.")
 
     def show_menu(self):
-        print("Deposit: press 1, Withdrawal: press 2, Transactions history: press 3, Log out: press 4\n")
+        print("Check balance: press 1, Deposit: press 2, Withdrawal: press 3, Transactions history: press 4, Log out: press 5")
+        menu_response = input()
+        print()
+        if menu_response == "1":
+            self.check_balance()
+        elif menu_response == "2":
+            self.deposit()
+        elif menu_response == "3":
+            self.withdra()
+        elif menu_response == "4":
+            print("transactions")
+        elif menu_response == "5":
+            print("Exit")
+        else:
+            print("Invalid value! Please choose a value from the menu.")
+            self.show_menu()
 
     def check_balance(self):
         print(f"Your balance is {self.balance} sek.\n") 
         self.show_menu()  
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            self.transactions.append("Deposit", amount)
+    def deposit(self):
+        try:
+            amount = float(input("Please enter the amount you want to deposit to your account here:\n"))
+            print()
+            if amount > 0:
+                self.balance += round(amount, 2)
+                time = datetime.now().strftime("%Y-%m-%d %H:%M")
+                self.transactions.append([int(self.account_number), "Deposit", round(amount, 2), time])
+                self.update_transactions()
+                self.update_balance()
+            else:
+                print("Please enter an amount greater than 0 sek.")
+                self.deposit()
+        except ValueError:
+            print("Invalid input, please enter a valid number.")
+            self.deposit()
 
 
-    def withdra(self, amount):
-        if amount > 0 and amount <= self.balance:
-            self.balance -= amount
-            self.transactions.append("Withdrawal", amount)
+    def withdra(self):
+        try:
+            amount = float(input("Please enter the amount you want to withdra from your account here:\n"))
+            print()
+            if amount > 0 and amount <= self.balance:
+                self.balance -= round(amount, 2)
+                time = datetime.now().strftime("%Y-%m-%d %H:%M")
+                self.transactions.append([int(self.account_number), "Withdrawal", round(amount,2), time])
+                self.update_transactions()
+                self.update_balance()
+            elif amount < 0:
+                print("Please enter an amount greater than 0 sek.")
+                self.withdra()
+            else:
+                print("Not enough bank account balance for this request. Please enter a valid value.")
+                self.withdra()
+        except ValueError:
+            print("Invalid input, please enter a valid number.")
+            self.withdra()
+    
+    def update_transactions(self):
+        transactions = self.transactions[0]
+        SHEET.worksheet("transactions").append_row(transactions)
+    
+    def update_balance(self):
+        SHEET.worksheet("user_details").update_cell(self.row_number, 6, f"{self.balance:.2f}")
+        print(f"Transaction successful. Current Balance: {self.balance:.2f} sek")
+
+
+
+
 
 class NewAccount(BankAccount):
     """
@@ -153,18 +208,21 @@ def account_validation(personal_ID, pin_code):
     try:
         cell = user_details.find(personal_ID)
         data = user_details.row_values(cell.row)
+        row_number = cell.row
         if data[2] == pin_code:
-            costumer = BankAccount(data[0], data[1], data[2], data[3], data[4], float(data[5]) )
+            costumer = BankAccount(data[0], data[1], data[2], data[3], data[4], row_number, float(data[5]))
             costumer.welcome_message()
         else:
             print("Wrong PIN code.")   
-    except:
+    except gspread.exceptions.CellNotFound:
         print("Account doesn't exist. Would you like to create a new account? (yes/no)")
         response = input().strip().lower()
         if response == "yes":
             NewAccount(pin_code, personal_ID)
         else:
             print("Exit app")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 get_login_inputs()
